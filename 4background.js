3015dba0b004d207cc1a978ba8d690f3914d888b
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-08-14 04:27:51 +0900

    4background fin
    final

--- 4background.js
+++ 4background.js
@@ -1,5 +1,4 @@
 const images = ["img1.png", "img2.png", "img3.jpg", "img4.png", "img5.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+document.body.style.backgroundSize = "cover";
