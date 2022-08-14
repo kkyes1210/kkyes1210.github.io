4898933b5c33dd3ca98254840f38dbdb0ee448cb
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2022-08-14 15:51:19 +0900

    final commit

---
+++ 2login.js
@@ -0,0 +1,38 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  const sen = [
+    "Hello, ",
+    "Have a good day, ",
+    "Nice to meet you, ",
+    "Welcom, ",
+  ];
+  greeting.innerText = `${
+    sen[Math.floor(Math.random() * sen.length)]
+  }${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
