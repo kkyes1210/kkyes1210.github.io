1b6cbea2cdba1718ae532d56f08e4851467cd42a
kkyes1210 <81374161+kkyes1210@users.noreply.github.com>
2023-02-28 02:33:38 +0900

    io test

--- 4background.js
+++
@@ -1,4 +0,0 @@
-const images = ["img1.png", "img2.png", "img3.jpg", "img4.png", "img5.jpg"];
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-document.body.style.backgroundImage = `url(img/${chosenImage})`;
-document.body.style.backgroundSize = "cover";
--- README.md
+++
@@ -1,3 +0,0 @@
-# kkyes1210.github.io
-<h3>Vanilla JS</h3>
-<p>Nomardcoder challenge</p>
